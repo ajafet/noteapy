f038e9f15fea4cda13de1031a279a5c0272067d0
Angel Delgado <43304091+ajafet@users.noreply.github.com>
2020-08-10 23:39:23 -0500

    Search Feature Update

--- noteapy/main/views/search.py
+++ noteapy/main/views/search.py
@@ -11,6 +11,10 @@ from main.forms import NotesForm
 from django.http import HttpResponse
 from django.db.models import Q
 
+## TO DO ##
+# Pagination Integration
+# Favorite Feature 
+# Transfer Feature 
 
 class SearchNotes(LoginRequiredMixin, ListView): 
     template_name = "main/search/search_notes.html"
@@ -19,55 +23,24 @@ class SearchNotes(LoginRequiredMixin, ListView):
 
     def get_queryset(self, **kwargs):
 
-        # Get Search Text and Option 
+        # Get Search Text
         search_text = self.request.GET.get('search_text')
-        search_option = self.request.GET.get('search_option')
 
-        if search_option == 1: 
-
-            # Get Notes 
+        if search_text: 
             object_list = self.model.objects.filter(Q(title__contains=search_text) | Q(content__contains=search_text)).order_by('id')
-
-        elif search_option == 2: 
-
-            # Get Categories 
-            object_list = Categories.objects.filter(Q(name__contains=search_text)).order_by('id')
-
-            categories = []
-
-            for obj in object_list: 
-                notes_count = Notes.objects.filter(category=obj).count()
-                categories.append({"categories": obj, "notes_count": notes_count})
-
-            object_list = categories 
-
         else: 
-
-            # Get Nothing 
-            object_list = [] 
-
+            object_list = []
 
         return object_list
 
 
     def get_context_data(self, **kwargs):    
 
-        # Get Request Data
+        # Making URL Variable Available In Template
+
         search_text = self.request.GET.get('search_text')
-        search_option = self.request.GET.get('search_option')
         
         context = super().get_context_data(**kwargs)
-
-        # Getting Search Option From URL and Passing Through Context 
-        if search_option == 1:             
-            context["search_option"] = 1
-        elif search_option == 2: 
-            context["search_option"] = 2
-        
-        # Determine If First Time Loading Page or Nothing Found
-        if len(search_text) == 0: 
-            context["nothing_found"] = 1 
-        else: 
-            context["nothing_found"] = 0 
+        context["search_text"] = search_text        
 
         return context

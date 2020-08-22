5fda36b82a5468b29af6ce97451181e8d9bc9271
Angel Delgado <43304091+ajafet@users.noreply.github.com>
2020-08-21 21:41:42 -0500

    Update Final?

--- noteapy/noteapy/settings.py
+++ noteapy/noteapy/settings.py
@@ -14,10 +14,6 @@ import os
 import dj_database_url
 from decouple import config
 
-# White Noise Stuff For Heroku Deployment 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

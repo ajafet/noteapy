18e3f338197cffd3ad49410a8872e0879e40e521
Angel Delgado <43304091+ajafet@users.noreply.github.com>
2020-08-21 20:45:47 -0500

    Update settings.py

--- noteapy/noteapy/settings.py
+++ noteapy/noteapy/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.whitenoise'
 ]
 
 ROOT_URLCONF = 'noteapy.urls'

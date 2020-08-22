a77595b42bd632fbe6629a9a4a4acce1715111df
Angel Delgado <43304091+ajafet@users.noreply.github.com>
2020-08-21 20:42:16 -0500

    Update settings.py

--- noteapy/noteapy/settings.py
+++ noteapy/noteapy/settings.py
@@ -18,7 +18,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 from dj_database_url import dj_database_url
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noteapy.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)

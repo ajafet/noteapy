2ea98d2b404819b04c7cd20e19828ea8e2fdbf52
Angel Delgado <43304091+ajafet@users.noreply.github.com>
2020-08-21 23:16:15 -0500

    Update

--- noteapy/noteapy/settings.py
+++ noteapy/noteapy/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-from decouple import config
-import django_heroku
-
+from decouple import config 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'noteapy.urls'
@@ -142,6 +141,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
